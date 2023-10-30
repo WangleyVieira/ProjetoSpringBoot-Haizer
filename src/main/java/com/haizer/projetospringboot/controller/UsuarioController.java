d59228a67f985ca31007c9ee2b3ce47d04497965
WangleyVieira <wangleymsv10@hotmail.com>
2023-10-30 15:10:50 -0400

    salvar usuario

--- src/main/java/com/haizer/projetospringboot/controller/UsuarioController.java
+++ src/main/java/com/haizer/projetospringboot/controller/UsuarioController.java
@@ -1,5 +1,56 @@
 package com.haizer.projetospringboot.controller;
 
-public class UsuarioController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.haizer.projetospringboot.modelo.Usuario;
+import com.haizer.projetospringboot.repository.UsuarioRepository;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("/novo")
+	public String adicionarUsuario(Model model) 
+	{
+		try {
+			model.addAttribute("usuario", new Usuario());
+			
+			return "/publica-criar-usuario";
+		}
+		catch(Exception e) {
+			return e.getMessage();
+		}
+		
+	}
+	
+	@PostMapping("/salvar")
+	public String salvarUsuario(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes)
+	{
+		try {
+			if (result.hasErrors()) {
+				return "/publica-criar-usuario";
+			}
+			
+			usuarioRepository.save(usuario);
+			attributes.addFlashAttribute("mensagem", "Usuário cadastrado com sucesso!");
+			
+			return "redirect:/usuario/novo";
+			
+		}
+		catch(Exception e) {
+			return e.getMessage();
+		}
+	}
 }
---
+++ src/main/java/com/haizer/projetospringboot/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.haizer.projetospringboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.haizer.projetospringboot.modelo.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
