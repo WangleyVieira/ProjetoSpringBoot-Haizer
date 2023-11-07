4db07a317c595dc915ead48a4c462e76f0726b40
WangleyVieira <wangleymsv10@hotmail.com>
2023-11-07 15:52:27 -0400

    deletar usuario

--- src/main/java/com/haizer/projetospringboot/controller/UsuarioController.java
+++ src/main/java/com/haizer/projetospringboot/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -67,4 +68,23 @@ public class UsuarioController {
 			return e.getMessage();
 		}
 	}
+	
+	@GetMapping("/admin/apagar/{id}")
+	public String deleteUser(@PathVariable("id") long id, Model model, RedirectAttributes attributes)
+	{
+		try {
+			Usuario usuario = usuarioRepository.findById(id)
+							.orElseThrow(() -> new IllegalArgumentException("Id inválido:" + id));
+			usuarioRepository.delete(usuario);
+			
+			attributes.addFlashAttribute("mensagem", "Usuário excluído com sucesso!");
+			return "redirect:/usuario/admin/listar";
+			
+		}
+		catch(Exception e) {
+			return e.getMessage();
+		}
+	}
+	
+	
 }
